e3cd102fb629ae670b65f4b89e1e5d9a1bf8a92d
Charles Roberson <crobshere@gmail.com>
2021-11-22 14:29:28 -0600

    Update kilo.c
    Turned off canonical mode

--- kilo.c
+++ kilo.c
@@ -5,13 +5,23 @@
 // Author: Charles Roberson
 //
 
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode(){
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode(){
-	struct termios raw;
-	tcgetattr(STDIN_FILENO, &raw);
-	raw.c_lflag &= ~(ECHO);
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+	
+	struct termios raw = orig_termios;
+	raw.c_lflag &= ~(ECHO | ICANON);
+	
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
