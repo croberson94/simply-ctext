29587af142f61167973060518bbb3648464137b2
Charles Roberson <crobshere@gmail.com>
2021-11-22 15:02:26 -0600

    Update kilo.c
    Display kepresses

--- kilo.c
+++ kilo.c
@@ -5,6 +5,8 @@
 // Author: Charles Roberson
 //
 
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -28,6 +30,13 @@ void enableRawMode(){
 int main(){
 	enableRawMode();
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+		if (iscntrl(c)){
+			printf("%d\n", c);
+		}
+		else{
+			printf("%d ('%c')\n", c, c);
+		}
+	}
 	return 0;
 }
