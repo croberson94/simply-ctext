ab1087471c5a52c5a26fb8ad2240f2635d840365
Charles Roberson <crobshere@gmail.com>
2021-11-23 17:09:19 -0600

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -29,6 +29,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
+	BACKSPACE = 127,
 	ARROW_LEFT = 1000,
 	ARROW_RIGHT,
 	ARROW_UP,
@@ -225,6 +226,24 @@ void editorAppendRow(char *s, size_t len){
 	editorUpdateRow(&E.row[at]);
 	
 	E.numrows++;
+}
+
+void editorRowInsertChar(erow *row, int at, int c){
+	if (at < 0 || at > row->size) at = row->size;
+	row->chars = realloc(row->chars, row->size + 2);
+	memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+	row->size++;
+	row->chars[at] = c;
+	editorUpdateRow(row);
+}
+
+// EDITOR OPERATIONS
+void editorInsertChar(int c){
+	if (E.cy == E.numrows){
+		editorAppendRow("", 0);
+	}
+	editorRowInsertChar(&E.row[E.cy], E.cx, c);
+	E.cx++;
 }
 
 // FILE IO
@@ -424,6 +443,10 @@ void editorMoveCursor(int key){
 void editorProcessKeypress(){
 	int c = editorReadKey();
 	switch (c){
+		case '\r':
+			/* TODO */
+			break;
+			
 		case CTRL_KEY('q'):
 			write(STDOUT_FILENO, "\x1b[2J", 4);
 			write(STDOUT_FILENO, "\x1b[H", 3);
@@ -439,6 +462,12 @@ void editorProcessKeypress(){
 				E.cx = E.row[E.cy].size;
 			break;
 			
+		case BACKSPACE:
+		case CTRL_KEY('h'):
+		case DEL_KEY:
+			/* TODO */
+			break;
+		
 		case PAGE_UP:
 		case PAGE_DOWN:
 		{
@@ -459,6 +488,14 @@ void editorProcessKeypress(){
 		case ARROW_LEFT:
 		case ARROW_RIGHT:
 			editorMoveCursor(c);
+			break;
+			
+		case CTRL_KEY('l'):
+		case '\x1b':
+			break;
+		
+		default:
+			editorInsertChar(c);
 			break;
 	}
 }
