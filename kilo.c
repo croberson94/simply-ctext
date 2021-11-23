17e472846a32fa055a412943fc54a741ed370938
Charles Roberson <crobshere@gmail.com>
2021-11-23 11:07:19 -0600

    Update kilo.c
    Append buffer to eliminate flicker effect for  screen

--- kilo.c
+++ kilo.c
@@ -10,6 +10,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -85,7 +86,7 @@ int getCursorPosition(int *rows, int *cols){
 int getWindowSize(int *rows, int *cols){
 	struct winsize ws;
 	
-	if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
 		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
 		return getCursorPosition(rows, cols);
 	}
@@ -96,11 +97,36 @@ int getWindowSize(int *rows, int *cols){
 	}
 }
 
+// APPEND BUFFER
+struct abuf {
+	char *b;
+	int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len){
+	char *new = realloc(ab->b, ab->len + len);
+	
+	if (new == NULL) return;
+	memcpy(&new[ab->len], s, len);
+	ab->b = new;
+	ab->len += len;
+}
+
+void abFree(struct abuf *ab){
+	free(ab->b);
+}
+
 // OUTPUT
 void editorDrawRows(){
 	int y;
 	for (y = 0; y < E.screenrows; y++){
-		write(STDOUT_FILENO, "~\r\n", 3);
+		write(STDOUT_FILENO, "~", 1);
+		
+		if (y < E.screenrows - 1){
+			write(STDOUT_FILENO, "\r\n", 2);
+		}
 	}
 }
 
