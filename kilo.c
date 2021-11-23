801c44f83b9474af70d7715d41bdeaf5f2566846
Charles Roberson <crobshere@gmail.com>
2021-11-23 13:52:41 -0600

    Scrollwheel update
    Adding scrollwheel functionality for the text editor

--- kilo.c
+++ kilo.c
@@ -45,6 +45,7 @@ typedef struct erow {
 
 struct editorConfig{
 	int cx, cy;
+	int rowoff;
 	int screenrows;
 	int screencols;
 	int numrows;
@@ -218,10 +219,20 @@ void abFree(struct abuf *ab){
 }
 
 // OUTPUT
+void editorScroll(){
+	if (E.cy < E.rowoff){
+		E.rowoff = E.cy;
+	}
+	if (E.cy >= E.rowoff + E.screenrows){
+		E.rowoff = E.cy - E.screenrows + 1;
+	}
+}
+
 void editorDrawRows(struct abuf *ab){
 	int y;
 	for (y = 0; y < E.screenrows; y++){
-		if (y >= E.numrows){
+		int filerow = y + E.rowoff;
+		if (filerow >= E.numrows){
 			if (E.numrows == 0 && y == E.screenrows / 3) {
 				char welcome[80];
 				int welcomelen = snprintf(welcome, sizeof(welcome),
@@ -240,9 +251,9 @@ void editorDrawRows(struct abuf *ab){
 			}
 		}
 		else{
-		int len = E.row[y].size;
+		int len = E.row[filerow].size;
 		if (len > E.screencols) len = E.screencols;
-		abAppend(ab, E.row[y].chars, len);
+		abAppend(ab, E.row[filerow].chars, len);
 		}
 		abAppend(ab, "\x1b[K", 3);
 		if (y < E.screenrows - 1){
@@ -252,6 +263,8 @@ void editorDrawRows(struct abuf *ab){
 }
 
 void editorRefreshScreen(){
+	editorScroll();
+	
 	struct abuf ab = ABUF_INIT;
 	abAppend(&ab, "\x1b[?2Jl", 6);
 	abAppend(&ab, "\x1b[H", 3);
@@ -287,7 +300,7 @@ void editorMoveCursor(int key){
 			}
 			break;
 		case ARROW_DOWN:
-			if (E.cy != E.screenrows - 1){
+			if (E.cy < E.numrows){
 				E.cy++;
 			}
 			break;
@@ -332,6 +345,7 @@ void editorProcessKeypress(){
 void initEditor(){
 	E.cx = 0;
 	E.cy = 0;
+	E.rowoff = 0;
 	E.numrows = 0;
 	E.row = NULL;
 	
